7b1457f8f628b76b47a5c663c4d4e7a7764db1d3
huangyisan <15783167@qq.com>
2017-02-27 15:06:42 +0800

    get external ip

--- ip_ping.py
+++ ip_ping.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+import etcd
+import socket
+
+def get_external_ip():
+	localIP = socket.gethostbyname(socket.gethostname())
+	ipList = socket.gethostbyname_ex(socket.gethostname())[-1]
+	for ip in ipList:
+		if ip != localIP:
+			return ip
+
+get_external_ip()
