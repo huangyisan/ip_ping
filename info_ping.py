da861ec21154d972af7decf63763763de2fa56cc
huangyisan <15783167@qq.com>
2017-02-27 16:55:54 +0800

    dict

--- info_ping.py
+++ info_ping.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import etcd
+import json
 
 
 
@@ -7,10 +8,15 @@ def create_etcd_client():
 	global client
 	client = etcd.Client(host='192.168.174.130',port=2379)
 
-def get_etcd_data():
-	data=client.read('/nodes/'+get_hostname(),recursive=True, sorted=True)
+def get_etcd_ip_data():
+	data=client.read('/nodes',recursive=True, sorted=True)
 	for i in data.children:
-		print i.value
+		dic = i.__dict__
+		global hostname ip
+		hostname=dic.get('key').split('/')[-1]
+		ip=dic.get('value')
+
+def ping_info_push():
 
 create_etcd_client()	
-get_etcd_data()
+get_etcd_ip_data()
